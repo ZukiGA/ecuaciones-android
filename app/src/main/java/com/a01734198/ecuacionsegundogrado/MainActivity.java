9eafa34f3ec4de79a8e07d35bc5fedee86fdddbe
ZukiGA <gonzalezarellanobryan@gmail.com>
2021-08-24 21:30:13 -0500

    Final commit

--- app/src/main/java/com/a01734198/ecuacionsegundogrado/MainActivity.java
+++ app/src/main/java/com/a01734198/ecuacionsegundogrado/MainActivity.java
@@ -2,6 +2,7 @@ package com.a01734198.ecuacionsegundogrado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     EditText root1;
     EditText root2;
     Solver solver;
+    AlertDialog.Builder builder;
+    AlertDialog alert;
 
 
     @Override
@@ -27,16 +30,43 @@ public class MainActivity extends AppCompatActivity {
         cTxt = findViewById(R.id.cTxt);
         root1 = findViewById(R.id.root1);
         root2 = findViewById(R.id.root2);
+        createAlert();
+
+    }
+
+    public void createAlert() {
+        builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Aviso");
+        builder.setMessage("No es una ecuación de segundo grado");
+        builder.setPositiveButton("Aceptar", null);
+        alert = builder.create();
     }
 
     public void solve(View v) {
-        Double a = Double.valueOf(aTxt.getText().toString());
-        Double b = Double.valueOf(bTxt.getText().toString());
-        Double c = Double.valueOf(cTxt.getText().toString());
-        String root1Txt = solver.getRoot1(a, b, c);
-        String root2Txt = solver.getRoot2(a, b, c);
-        root1.setText(root1Txt);
-        root2.setText(root2Txt);
+        String aText = aTxt.getText().toString();
+        String bText = bTxt.getText().toString();
+        String cText = cTxt.getText().toString();
+
+        if (bText.length() == 0) bText = "0";
+        if (cText.length() == 0) cText = "0";
+        if (aText.length() == 0) aText = "0";
+
+        Double a = Double.valueOf(aText);
+        Double b = Double.valueOf(bText);
+        Double c = Double.valueOf(cText);
+
+        if (a == 0) {
+            alert.show();
+        } else {
+            String root1Txt = solver.getRoot1(a, b, c);
+            String root2Txt = solver.getRoot2(a, b, c);
+            root1.setText(root1Txt);
+            root2.setText(root2Txt);
+        }
+
+
+
+
     }
 
 
--- app/src/main/java/com/a01734198/ecuacionsegundogrado/Solver.java
+++ app/src/main/java/com/a01734198/ecuacionsegundogrado/Solver.java
@@ -2,12 +2,32 @@ package com.a01734198.ecuacionsegundogrado;
 
 public class Solver {
     public String getRoot1(double a, double b, double c) {
-        String root1 = String.valueOf((-b + Math.sqrt(Math.pow(b,2)-4 * a * c))/(2*a));
-        return root1;
+        Double root = getSquareRoot(a, b, c);
+        if (root < 0) {
+            double real = -b / (2.0 * a);
+            double imaginary = (Math.sqrt(root * -1.0)/ (2.0 * a));
+            String complex = String.valueOf(real) + " + " + String.format("%.2f",imaginary) + "i";
+            return complex;
+        } else {
+            String root1 = String.format("%.2f",(-b + Math.sqrt(root))/( 2.0 * a));
+            return root1;
+        }
     }
 
     public String getRoot2(double a, double b, double c) {
-        String root1 = String.valueOf((-b - Math.sqrt(Math.pow(b,2)-4 * a * c))/(2*a));
-        return root1;
+        Double root = getSquareRoot(a, b, c);
+        if (root < 0) {
+            double real = -b / (2.0 * a);
+            double imaginary = (Math.sqrt(root * - 1.0)/ (2.0 * a));
+            String complex = String.valueOf(real) + " - " + String.format("%.2f",imaginary) + "i";
+            return complex;
+        } else {
+            String root2 = String.format("%.2f",(-b - Math.sqrt(root))/( 2.0 * a));
+            return root2;
+        }
+    }
+
+    public Double getSquareRoot(double a, double b, double c) {
+        return Math.pow(b,2.0) - 4.0 * a * c;
     }
 }
